f7f9307aeb392e3bf1823901a1874c5a5c658ffa
Req-kun <69065737+Req-kun@users.noreply.github.com>
2021-05-22 11:58:51 +0900

    add source command

---
+++ cogs/source.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from inspect import getsourcelines
+from os.path import relpath
+
+source_url = 'https://github.com/Req-kun/chozatu'
+branch = 'main'
+
+
+class Source(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def source(self, ctx, *, command):
+        if command is None:
+            return await ctx.send(source_url)
+
+        obj = self.bot.get_command(command.replace('.', ' '))
+        if obj is None:
+            return await ctx.send('Could not find command.')
+
+        src = obj.callback.__code__
+        module = obj.callback.__module__
+        filename = src.co_filename
+
+        lines, firstlineno = getsourcelines(src)
+        if not module.startswith('discord'):
+            location = relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
+            source_url = 'https://github.com/Rapptz/discord.py'
+            branch = 'master'
+        await ctx.send(f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>')
+
+def setup(bot):
+    bot.add_cog(Source(bot))
